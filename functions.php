e1c2890b32b2da4f12a148968c0db756be0065a0
Jesse Nickles <jesse@littlebizzy.com>
2023-01-19 15:52:04 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
-function enqueue_parent_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
-
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+// enqueue parent css
+if ( !function_exists( 'enqueue_parent_css' ) ):
+    function enqueue_parent_css() {
+        wp_enqueue_style( 'parent-css', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_css', 10 );
