42a5dc75851e216b04844be3bbea5de2ac93d350
Jesse Nickles <jesse@littlebizzy.com>
2023-01-19 15:58:11 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+// child rtl css
+if ( !function_exists( 'child_rtl_css' ) ):
+    function child_rtl_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'child_rtl_css' );
+
 // enqueue parent css
 if ( !function_exists( 'enqueue_parent_css' ) ):
     function enqueue_parent_css() {
