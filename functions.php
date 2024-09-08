61d2c17106aaf67359487cac50cccdfa2ef445e3
Jesse Nickles <jesse@littlebizzy.com>
2024-09-08 18:05:11 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,19 +1,13 @@
 <?php
 
-// child rtl css
-if ( !function_exists( 'child_rtl_css' ) ):
-    function child_rtl_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
-            $uri = get_template_directory_uri() . '/rtl.css';
-        return $uri;
+// Child RTL CSS for HoverCraft theme
+function hovercraft_child_rtl_css( $uri ) {
+    if ( is_rtl() && file_exists( get_stylesheet_directory() . '/rtl.css' ) ) {
+        $uri = get_stylesheet_directory_uri() . '/rtl.css';
     }
-endif;
-add_filter( 'locale_stylesheet_uri', 'child_rtl_css' );
 
-// enqueue parent css
-if ( !function_exists( 'enqueue_parent_css' ) ):
-    function enqueue_parent_css() {
-        wp_enqueue_style( 'parent-css', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_css', 10 );
+    return $uri;
+}
+add_filter( 'locale_stylesheet_uri', 'hovercraft_child_rtl_css' );
+
+// Ref: ChatGPT
